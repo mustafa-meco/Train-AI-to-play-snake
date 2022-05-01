1775054b66415efd41bf459644246232dc0fc90f
Mustafa <musgamal05@gmail.com>
2022-05-01 07:52:57 +0200

    small change in the score print

--- snake_game.py
+++ snake_game.py
@@ -126,7 +126,7 @@ class SnakeGameAI():
 
         pygame.draw.rect(self.display, RED, pygame.Rect(self.food.x, self.food.y, BLOCK_SIZE, BLOCK_SIZE))
 
-        text = font.render("Score: " + str(self.score), True, WHITE)
+        text = font.render(f"Score: {str(self.score)}", True, WHITE)
         self.display.blit(text, [0,0])
         pygame.display.flip()
 
